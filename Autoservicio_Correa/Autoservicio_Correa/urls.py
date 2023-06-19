0682ebc6aacb6121d653f66003ea45e7ac0a7381
FrancoMath <frankito_mathier@hotmail.com>
2023-06-19 12:29:09 -0300

    Login, register y logout creados

--- Autoservicio_Correa/Autoservicio_Correa/urls.py
+++ Autoservicio_Correa/Autoservicio_Correa/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('AppCorrea/', include('AppCorrea.urls'))
+    path('AppCorrea/', include('AppCorrea.urls')),
+    path('users/', include('users.urls'))
 ]
