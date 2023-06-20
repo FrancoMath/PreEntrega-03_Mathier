446bef642ed9bf395fe6c1121f33c881d6da6d27
FrancoMath <frankito_mathier@hotmail.com>
2023-06-20 10:25:38 -0300

    Acomodando base.html

--- Autoservicio_Correa/AppCorrea/views.py
+++ Autoservicio_Correa/AppCorrea/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Producto, Cliente, Pedido_Basico
 
+#from django.contrib.auth.decorators import login_required
+#@login_required
+
 def inicio(request):
     return render(request, "AppCorrea/index.html")
+
+
 
 def productos(request):
     if request.method == 'POST':
